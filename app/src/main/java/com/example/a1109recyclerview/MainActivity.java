c7b8a62635de36b233ac7adc93e00a6591c4dda3
yoyo <ya90312@gmail.com>
2021-11-09 19:29:28 +0800

    完成主畫面recyclerView之繫結

--- app/src/main/java/com/example/a1109recyclerview/MainActivity.java
+++ app/src/main/java/com/example/a1109recyclerview/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.a1109recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rcv = findViewById(R.id.rcv);
+        ArrayList<Data> data = new ArrayList<>();
+        data.add(new Data(1, "test"));
+        Myadapter adapter = new Myadapter(this, data);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+        rcv.setAdapter(adapter);
+
     }
 }
